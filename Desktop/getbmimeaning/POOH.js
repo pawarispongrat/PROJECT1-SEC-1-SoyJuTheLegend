038d2c58ae5204493342f63cdc34863fc27c3cc5
POOHHO <patcharaphol.pooh@mail.kmutt.ac.th>
2022-10-08 23:33:16 +0700

    node

---
+++ Desktop/getbmimeaning/POOH.js
@@ -0,0 +1,35 @@
+// function* even(n) {
+//   let index = 0
+
+//   while (index <= n) {
+//     yield index
+
+//     index += 2
+//   }
+// }
+
+// const evenGenerator = even(10)
+
+// for (let i of evenGenerator) {
+//   console.log(i)
+// }
+
+// const arr = ['B', 'a', 'b', 'e', 'l']
+
+// for (let key in arr) {
+//   console.log(key)
+
+// }
+
+// let marks = [ 4, 5, 7, 9, 10, 3 ];
+
+// let lessThanFive = false;
+
+// for (let index = 0; index < marks.length; index++) {
+//     if (marks[index] < 5) {
+//         lessThanFive = true;
+//         break;
+//     }
+// }
+
+// console.log(lessThanFive);
